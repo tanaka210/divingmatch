bc656f9a7025b202a103250fcb6d47b6577c9dcc
Ubuntu <ubuntu@ip-172-31-43-63.ap-northeast-1.compute.internal>
2020-10-25 11:54:21 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,20 +1,35 @@
-<header class="mb-4">
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        {{-- トップページへのリンク --}}
-        <a class="navbar-brand" href="/">Anybuddy</a>
+<!--<header class="mb-4">-->
+<!--    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">-->
+<!--        {{-- トップページへのリンク --}}-->
+<!--        <a class="navbar-brand" href="/">Anybuddy</a>-->
 
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<!--        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">-->
+<!--            <span class="navbar-toggler-icon"></span>-->
+<!--        </button>-->
 
-        <div class="collapse navbar-collapse" id="nav-bar">
-            <ul class="navbar-nav mr-auto"></ul>
-            <ul class="nav navbar-nav navbar-right">
-                    {{-- ユーザ登録ページへのリンク --}}
-                    <li>{!! link_to_route('signup.get', '新規登録', [], ['class' => 'nav-link']) !!}</li>
-                    {{-- ログインページへのリンク --}}
-                    <li><a href="#">ログイン</a></li>
-            </ul>
-        </div>
-    </nav>
-</header>
+<!--        <div class="collapse navbar-collapse" id="nav-bar">-->
+<!--            <ul class="navbar-nav mr-auto"></ul>-->
+<!--            <ul class="navbar-nav">-->
+<!--                @if (Auth::check())-->
+<!--                    {{-- ユーザ一覧ページへのリンク --}}-->
+<!--                    <li class="nav-item"><a href="#" class="nav-link">Users</a></li>-->
+<!--                    <li class="nav-item dropdown">-->
+<!--                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>-->
+<!--                        <ul class="dropdown-menu dropdown-menu-right">-->
+<!--                            {{-- ユーザ詳細ページへのリンク --}}-->
+<!--                            <li class="dropdown-item"><a href="#">マイページ</a></li>-->
+<!--                            <li class="dropdown-divider"></li>-->
+<!--                            {{-- ログアウトへのリンク --}}-->
+<!--                            <li class="dropdown-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>-->
+<!--                        </ul>-->
+<!--                    </li>-->
+<!--                @else-->
+<!--                    {{-- ユーザ登録ページへのリンク --}}-->
+<!--                    <li class="nav-item">{!! link_to_route('signup.get', '新規登録', [], ['class' => 'nav-link']) !!}</li>-->
+<!--                    {{-- ログインページへのリンク --}}-->
+<!--                    <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>-->
+<!--                @endif-->
+<!--            </ul>-->
+<!--        </div>-->
+<!--    </nav>-->
+<!--</header>-->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
+　　@if (Auth::check())
+　　　　{{ Auth::user()->name }}
+　　@else
+    　　<div class="center jumbotron">
         <div class="text-center">
-            <h1>Welcome to the Anybuddy</h1>
-            {{-- ユーザ登録ページへのリンク --}}
-            {!! link_to_route('signup.get', '新規登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            <h1>Where is your buddy?</h1>
+            <p>一緒に潜るバディを探そう</p>
         </div>
-    </div>
+        
+            <div class="text-center">
+                <h2>ログイン</h2>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 offset-sm-3">
+                    {!! Form::open(['route' => 'login.post']) !!}
+                    <div class="form-group">
+                    {!! Form::label('email', 'メールアドレス') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                    </div>
+                    <div class="form-group">
+                    {!! Form::label('password', 'パスワード') !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                    </div>
+                    {!! Form::submit('GO!', ['class' => 'btn btn-primary btn-block']) !!}
+                    {!! Form::close() !!}
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <p class="mt-2">{!! link_to_route('signup.get', '新規登録') !!}</p>
+                </div>
+            </div>
+        </div>
+    @endif
 @endsection
